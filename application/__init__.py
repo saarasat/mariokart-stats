3cc3586b60c2576f9a568f1ac07ecc6273d7bb35
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-03-19 17:54:05 +0200

    Part 3: added a Heroku database

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mkraces.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mkraces.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -30,4 +35,7 @@ login_manager.login_message = "Please login to use this functionality"
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
