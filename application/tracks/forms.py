449dd79b62cba637c753b27e408909d634c50307
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-03-28 15:14:45 +0200

    Fixes to the track-form

--- application/tracks/forms.py
+++ application/tracks/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, validators
 
 class TrackForm(FlaskForm):
-    cup = SelectField("Cup", choices=[('Mushroom Cup', 'Mushroom Cup'), ('Flower Cup', 'Flower Cup') ('Star Cup', 'Star Cup'), ('Special Cup', 'Special Cup')])
+    cup = StringField("Cup", choices=[])
     name = SelectField("Track name", choices=[])
     
     class Meta:
