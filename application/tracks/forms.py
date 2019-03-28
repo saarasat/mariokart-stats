cf645997e2b9fc93f3b322f38450732d23b2a504
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-03-28 15:09:13 +0200

    Part 3: removing unnecessary elements

--- application/tracks/forms.py
+++ application/tracks/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, validators
 
 class TrackForm(FlaskForm):
-    cup = SelectField("Cup", choices=[('Star Cup', 'Star Cup'), ('Special Cup', 'Special Cup')])
+    cup = SelectField("Cup", choices=[('Mushroom Cup', 'Mushroom Cup'), ('Flower Cup', 'Flower Cup') ('Star Cup', 'Star Cup'), ('Special Cup', 'Special Cup')])
     name = SelectField("Track name", choices=[])
     
     class Meta:
