d9e5439a4e67ce4cd16a768ad0ac68c7a0c1e5f3
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-04-30 13:38:28 +0300

    Changes in initialization of the database

--- application/tracks/models.py
+++ application/tracks/models.py
@@ -10,9 +10,8 @@ class Track(Base):
     favoritetracks = db.relationship('Player', secondary=favoritetracks, backref=db.backref('tracks', lazy='dynamic'))
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
-    def __init__(self, name, account_id):
+    def __init__(self, name):
         self.name = name
-        self.account_id = account_id
         
     @staticmethod
     def tracks_basic_stats(id):
